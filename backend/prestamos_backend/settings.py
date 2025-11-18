d12b133ddaaf84a43d71960a43efe32cd1985902
brayan_trujillo10 <trujilloramonbrayan@gmail.com>
2025-11-18 11:35:31 -0500

    Arreglo de conflictos JWT

--- backend/prestamos_backend/settings.py
+++ backend/prestamos_backend/settings.py
@@ -108,15 +108,10 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Django REST Framework
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
 }
 
 # JWT Configuration
