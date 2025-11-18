189f7226239cb09a51d2e10812c4decc59dcda76
brayan_trujillo10 <trujilloramonbrayan@gmail.com>
2025-11-18 16:58:43 -0500

    Ultima Modificacion

--- backend/prestamos/serializers.py
+++ backend/prestamos/serializers.py
@@ -1,21 +1,72 @@
 from rest_framework import serializers
 from .models import Prestamo, Pago
-from usuarios.serializers import UserSerializer
+from django.contrib.auth.models import User
 
 class PrestamoSerializer(serializers.ModelSerializer):
-    estudiante = UserSerializer(read_only=True)
-    total_pagar = serializers.SerializerMethodField()
-    cuota_mensual = serializers.SerializerMethodField()
+    # Campos para lectura
+    estudiante_nombre = serializers.SerializerMethodField(read_only=True)
+    total_pagar = serializers.SerializerMethodField(read_only=True)
+    cuota_mensual = serializers.SerializerMethodField(read_only=True)
+    
+    # Campo para escritura
+    estudiante_id = serializers.IntegerField(write_only=True)
     
     class Meta:
         model = Prestamo
-        fields = '__all__'
+        fields = [
+            'id', 'estudiante_id', 'estudiante_nombre',
+            'monto_solicitado', 'monto_aprobado', 'tasa_interes', 
+            'plazo_meses', 'fecha_solicitud', 'fecha_aprobacion',
+            'fecha_desembolso', 'fecha_vencimiento', 'estado',
+            'motivo', 'observaciones', 'aprobado_por',
+            'total_pagar', 'cuota_mensual'
+        ]
+        read_only_fields = ['fecha_solicitud', 'id']
+    
+    def get_estudiante_nombre(self, obj):
+        return f"{obj.estudiante.first_name} {obj.estudiante.last_name}"
     
     def get_total_pagar(self, obj):
-        return obj.calcular_total_pagar()
+        return float(obj.calcular_total_pagar())
     
     def get_cuota_mensual(self, obj):
-        return obj.calcular_cuota_mensual()
+        return float(obj.calcular_cuota_mensual())
+    
+    def create(self, validated_data):
+        # Extraer estudiante_id
+        estudiante_id = validated_data.pop('estudiante_id')
+        
+        # Buscar el usuario
+        try:
+            estudiante = User.objects.get(id=estudiante_id)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({
+                'estudiante_id': f'Usuario con ID {estudiante_id} no existe'
+            })
+        
+        # Crear el préstamo con el estudiante
+        prestamo = Prestamo.objects.create(
+            estudiante=estudiante,
+            **validated_data
+        )
+        
+        return prestamo
+    
+    def to_representation(self, instance):
+        """Personalizar la respuesta para incluir datos del estudiante"""
+        representation = super().to_representation(instance)
+        
+        # Agregar info del estudiante
+        if instance.estudiante:
+            representation['estudiante'] = {
+                'id': instance.estudiante.id,
+                'username': instance.estudiante.username,
+                'first_name': instance.estudiante.first_name,
+                'last_name': instance.estudiante.last_name,
+                'email': instance.estudiante.email
+            }
+        
+        return representation
 
 class PagoSerializer(serializers.ModelSerializer):
     class Meta:
